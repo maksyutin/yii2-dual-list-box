e88c6c01f918ca04c47e0c45e875c2f61533f722
Дмитрий Максютин <maksyutin@yandex.ru>
2015-01-22 15:05:53 +0500

    Init commit

---
+++ Asset.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace maksyutin\widgets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for yii2-dual-list-box Widget
+ *
+ * @author Maksyutin Dmitriy <maksyutin@yandex.ru>
+ * @since 1.0
+ */
+class Asset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+    /**
+     * Set up CSS and JS asset arrays based on the base-file names
+     * @param string $type whether 'css' or 'js'
+     * @param array $files the list of 'css' or 'js' basefile names
+     */
+    protected function setupAssets($type, $files = [])
+    {
+        $srcFiles = [];
+        $minFiles = [];
+        foreach ($files as $file) {
+            $srcFiles[] = "{$file}.{$type}";
+            $minFiles[] = "{$file}.min.{$type}";
+        }
+        if (empty($this->$type)) {
+            $this->$type = YII_DEBUG ? $srcFiles : $minFiles;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+
+        $this->setupAssets('js', ['js/dual-list-box']);
+
+        parent::init();
+    }
+
+    /**
+     * Sets the source path if empty
+     * @param string $path the path to be set
+     */
+    protected function setSourcePath($path)
+    {
+        if (empty($this->sourcePath)) {
+            $this->sourcePath = $path;
+        }
+    }
+}
